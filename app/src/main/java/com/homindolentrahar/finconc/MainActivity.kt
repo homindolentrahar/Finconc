c5924df6777a2ced4e99b38de05eab71b067b69e
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2022-03-29 11:56:33 +0700

    Praktikum 4 - Fragment

--- app/src/main/java/com/homindolentrahar/finconc/MainActivity.kt
+++ app/src/main/java/com/homindolentrahar/finconc/MainActivity.kt
@@ -4,32 +4,39 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import androidx.fragment.app.Fragment
+import com.homindolentrahar.finconc.home.HomeFragment
+import com.homindolentrahar.finconc.profile.ProfileFragment
+import com.homindolentrahar.finconc.settings.SettingsFragment
 
 class MainActivity : AppCompatActivity() {
-    private val btnHome = findViewById<Button>(R.id.btn_home)
-    private val btnProfile = findViewById<Button>(R.id.btn_profile)
-    private val btnSettings = findViewById<Button>(R.id.btn_settings)
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnHome.setOnClickListener {
-            val intent = Intent(this, HomeActivity::class.java)
-            intent.putExtra("username", "Raharu")
+//        Init views
+        val btnHome = findViewById<Button>(R.id.btn_home)
+        val btnProfile = findViewById<Button>(R.id.btn_profile)
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
 
-            startActivity(intent)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, HomeFragment())
+            .commitNow()
+
+        btnHome.setOnClickListener {
+            loadFragment(HomeFragment())
         }
         btnProfile.setOnClickListener {
-            val intent = Intent(this, ProfileActivity::class.java)
-
-            startActivity(intent)
+            loadFragment(ProfileFragment())
         }
         btnSettings.setOnClickListener {
-            val intent = Intent(this, SettingsActivity::class.java)
-
-            startActivity(intent)
+            loadFragment(SettingsFragment())
         }
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.frame_layout, fragment)
+            .commitNow()
     }
 }
