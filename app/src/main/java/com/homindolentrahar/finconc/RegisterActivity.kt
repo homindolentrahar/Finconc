f086f5634793e3ece460680face2f9c3d46c2d1f
Rahardiyan E. Widiatmoko <rahardiyanekowidiatmoko@gmail.com>
2022-04-19 15:34:56 +0700

    Register activity

--- app/src/main/java/com/homindolentrahar/finconc/RegisterActivity.kt
+++ app/src/main/java/com/homindolentrahar/finconc/RegisterActivity.kt
@@ -1,11 +1,49 @@
 package com.homindolentrahar.finconc
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.homindolentrahar.finconc.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.btnBack.setOnClickListener {
+            finish()
+        }
+
+        binding.btnRegister.setOnClickListener {
+            val fullname = binding.editName.text.toString()
+            val email = binding.editEmail.text.toString()
+            val password = binding.editPassword.text.toString()
+
+            val bundle = Bundle().apply {
+                putString("fullname", fullname)
+                putString("email", email)
+                putString("password", password)
+            }
+
+            Intent(this, HomeActivity::class.java).apply {
+                putExtras(bundle)
+
+                startActivity(this)
+            }
+        }
+
+        binding.btnTextLogin.setOnClickListener {
+            Intent(this, LoginActivity::class.java).apply {
+                flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                startActivity(this)
+
+                finish()
+            }
+        }
     }
 }
